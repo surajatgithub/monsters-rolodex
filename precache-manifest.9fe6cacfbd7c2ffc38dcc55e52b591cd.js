1092fe3bdee916ab88dfbbac16630061abc168e2
Suraj Yadav <2018surajyadav96@gmail.com>
2020-06-25 11:24:49 +0530

    Updates

---
+++ precache-manifest.9fe6cacfbd7c2ffc38dcc55e52b591cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d640b7d94c56e47d2ca906c84725935",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5e17040b4c8bc9b3ed29",
+    "url": "/monsters-rolodex/static/css/main.d197cea4.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e17040b4c8bc9b3ed29",
+    "url": "/monsters-rolodex/static/js/main.b9d2853d.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
